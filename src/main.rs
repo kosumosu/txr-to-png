486a588f97d8158fa0f4500196245814b7de2f26
Kosmos <japangod@yandex.ru>
2025-08-15 14:20:07 +0300

    Added U8V8 "experimental" support. Some cleanup and prettification.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use nom::Err;
 use clap::Parser;
 use nom::bytes::complete::{tag, take};
-use nom::combinator::{map, map_opt, map_res};
+use nom::combinator::{map, map_res};
+use nom::error::ErrorKind;
 use nom::number::complete::{le_u32, le_u8};
+use nom::Err;
 use nom::IResult;
+use png::ColorType;
 use regex::Regex;
 use std::fmt::Display;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
-use nom::error::ErrorKind;
 use tinytga::{Bpp, RawPixel, RawTga};
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
@@ -72,9 +73,9 @@ async fn convert_file(input_file: impl AsRef<Path>, output_file: impl AsRef<Path
     let file_future = File::create(output_file);
 
     let convert_future = match format {
-        TxrFormat::Rgba4444 => convert_rgba4444(source_image),
-        TxrFormat::Rgb565 => convert_rgb565(source_image),
-        TxrFormat::Some8 => convert_rgb565(source_image), // Not clear how to transform, but anyway...
+        TxrFormat::Rgba4444 => convert(source_image, ColorType::Rgba, rgb4444_to_8888),
+        TxrFormat::Rgb565 => convert(source_image, ColorType::Rgb, rgb565_to_888),
+        TxrFormat::U8V8Maybe => convert(source_image, ColorType::Rgb, rgb88_to_888), // Not clear how to transform, but anyway...
         _ => panic!()
     };
     let (file_result, convert_result) = tokio::join!(file_future, convert_future);
@@ -86,37 +87,11 @@ async fn convert_file(input_file: impl AsRef<Path>, output_file: impl AsRef<Path
     file.write_all(&png_data).await.unwrap();
 }
 
-fn convert_rgb565(source_image: RawTga) -> JoinHandle<Vec<u8>> {
+fn convert<const N: usize>(source_image: RawTga<'_>, png_format: png::ColorType, convert: impl FnMut(RawPixel) -> [u8; N]) -> JoinHandle<Vec<u8>> {
     let rgb888_data = source_image
         .pixels()
-        .map(|pixel| rgb565_to_888(&pixel).into())
-        .flat_map(|(r, g, b)| [r, g, b])
-        .collect::<Vec<_>>();
-    let width = source_image.size().width;
-    let height = source_image.size().height;
-
-    let convert_future = tokio::task::spawn_blocking(move || {
-        let mut png_data: Vec<u8> = Vec::new();
-        // let writer = Cursor::new(png_data.as_mut_slice());
-        let mut png_encoder = png::Encoder::new(&mut png_data, width, height);
-
-        png_encoder.set_color(png::ColorType::Rgb);
-        png_encoder.set_compression(png::Compression::Best);
-        png_encoder.set_depth(png::BitDepth::Eight);
-        let mut png_writer = png_encoder.write_header().unwrap();
-
-        png_writer.write_image_data(rgb888_data.as_slice()).unwrap();
-        png_writer.finish().unwrap();
-        png_data
-    });
-    convert_future
-}
-
-fn convert_rgba4444(source_image: RawTga) -> JoinHandle<Vec<u8>> {
-    let rgb888_data = source_image
-        .pixels()
-        .map(|pixel| rgb4444_to_8888(&pixel).into())
-        .flat_map(|(r, g, b, a)| [r, g, b, a])
+        .map(convert)
+        .flatten()
         .collect::<Vec<_>>();
     let width = source_image.size().width;
     let height = source_image.size().height;
@@ -127,7 +102,7 @@ fn convert_rgba4444(source_image: RawTga) -> JoinHandle<Vec<u8>> {
         // let writer = Cursor::new(png_data.as_mut_slice());
         let mut png_encoder = png::Encoder::new(&mut png_data, width, height);
 
-        png_encoder.set_color(png::ColorType::Rgba);
+        png_encoder.set_color(png_format);
         png_encoder.set_compression(png::Compression::Best);
         png_encoder.set_depth(png::BitDepth::Eight);
         let mut png_writer = png_encoder.write_header().unwrap();
@@ -163,7 +138,7 @@ fn read_extension(input: &[u8]) -> IResult<&[u8], TxrFormat> {
 }
 
 fn obtain_extension_offset(input: &[u8]) -> IResult<&[u8], u32> {
-    let (input, id_len) = le_u8(input)?;
+    let (input, _id_len) = le_u8(input)?;
     let (input, _header) = take(17u32)(input)?;
     let (input, _magic_loff) = tag("LOFF")(input)?;
     // let (input, _magic_loff) = map_res(take(4u32), parse_assert_loff_magic)(input)?;
@@ -196,7 +171,7 @@ enum TxrFormat {
     Rgba4444 = 5,
     Some6 = 6,
     Some7 = 7,
-    Some8 = 8, // Used by bumpcoord
+    U8V8Maybe = 8, // Used by bumpcoord
 }
 
 
@@ -216,7 +191,7 @@ fn obtain_format_from_extension(input: &[u8]) -> IResult<&[u8], TxrFormat> {
 
         let format = match tag {
             ExtensionTag::PFRM => map(
-                |s| {
+                |input| {
                     let (input, v0) = le_u32(input)?;
                     let (input, v1) = le_u32(input)?;
                     let (input, v2) = le_u32(input)?;
@@ -231,7 +206,7 @@ fn obtain_format_from_extension(input: &[u8]) -> IResult<&[u8], TxrFormat> {
                         (3840, 240, 15, 61440) => TxrFormat::Rgba4444,
                         (16711680, 65280, 255, 0) => TxrFormat::Some6,
                         (16711680, 65280, 255, _) => TxrFormat::Some7,
-                        (_, _, _, _) => TxrFormat::Some8,
+                        (_, _, _, _) => TxrFormat::U8V8Maybe,
                     })
                 },
             )(input)?.1,
@@ -261,13 +236,6 @@ fn obtain_format_from_extension(input: &[u8]) -> IResult<&[u8], TxrFormat> {
     Ok((input, format))
 }
 
-fn parse_assert_loff_magic(magic: &[u8]) -> Result<(), TxrError> {
-    if magic != b"LOFF" {
-        return Err(TxrError::ParseError);
-    }
-    Ok(())
-}
-
 fn parse_extension_tag(input: &[u8]) -> Result<ExtensionTag, TxrError> {
     Ok(match input {
         b"PFRM" => ExtensionTag::PFRM,
@@ -278,17 +246,23 @@ fn parse_extension_tag(input: &[u8]) -> Result<ExtensionTag, TxrError> {
     })
 }
 
-fn rgb565_to_888(pixel: &RawPixel) -> (u8, u8, u8) {
+fn rgb565_to_888(pixel: RawPixel) -> [u8; 3] {
     let b: u8 = ((pixel.color << 3) & 0b1111_1000) as u8;
     let g: u8 = ((pixel.color >> 3) & 0b1111_1100) as u8;
     let r: u8 = ((pixel.color >> 8) & 0b1111_1000) as u8;
-    (r, g, b)
+    [r, g, b]
 }
 
-fn rgb4444_to_8888(pixel: &RawPixel) -> (u8, u8, u8, u8) {
+fn rgb4444_to_8888(pixel: RawPixel) -> [u8; 4] {
     let b: u8 = ((pixel.color << 4) & 0b1111_0000) as u8;
     let g: u8 = ((pixel.color << 0) & 0b1111_0000) as u8;
     let r: u8 = ((pixel.color >> 4) & 0b1111_0000) as u8;
     let a: u8 = ((pixel.color >> 8) & 0b1111_0000) as u8;
-    (r, g, b, a)
+    [r, g, b, a]
+}
+
+fn rgb88_to_888(pixel: RawPixel) -> [u8; 3] {
+    let u: u8 = ((pixel.color << 0) & 0xFF) as u8;
+    let v: u8 = ((pixel.color >> 8) & 0xFF) as u8;
+    [u, v, 0]
 }
